fd3e31df0c67e0c77f1e8fff34d8b219c94447bd
OpenShift guest <jessmand@mit.edu>
2013-06-11 16:18:45 -0400

    working calculator

--- apple-calculator.js
+++ apple-calculator.js
@@ -1,96 +1,129 @@
-//calculate: evaluate the the value of an arithmetical expression
-function calculate(text) {
-    var pattern = /\d+(\.\d+)?|\+|\-|\*|\/|\(|\)/g;
+function calculate(text){
+    var pattern = /\d*\.?\d+|\+|\-|\×|\÷|\(|\)|\w*\(/g;
     var tokens = text.match(pattern);
-    try {
-        var num_open = 0;
-		var num_close = 0;
-		if(tokens==null) {
-			if(isNaN(parseFloat(text))) throw "Number Expected";
-			else return text;
-		}
-		for(var i=0;i<tokens.length;i++) {
-			if (tokens[i]=='(') num_open++;
-			if (tokens[i]==')') num_close++;
-		}
-		if(num_open!=num_close) throw "Ill-formed expression.";
+    try{
         var val = evaluate(tokens);
-        //if(tokens.length>0) throw "Ill-formed expression.";
-		if(isNaN(val)) throw "Number expected";
-        return String(val)
+        if(tokens.length > 0) throw "ill-formed expression";
+        return String(val);
     }
-    catch(err) {
-        return "Error: "+err;
+    catch(err){
+        return err;
     }
 }
 
-//looks for integer as next item and handles for open parentheses
-function read_operand(tokens) {
-    var first = tokens.shift();
-        if(first=="(") {
-            try {
-                if(tokens.indexOf(")")==-1) throw "Missing close-parentheses."
-            }
-            catch(err) {
-                return "Error: "+err;
-            }
-            var newInt = evaluate(tokens);
-        } else if(first=="-") {
-			var newInt = -1*parseFloat(tokens.shift());
-		}
-		else {
-            var newInt = parseFloat(first);
+function read_operand(tokens){
+    try{
+        var num = tokens.shift();
+        if (num == '('){
+            num = evaluate(tokens);
+            if(tokens.shift() != ')') throw "mismatched";
+            return num;
         }
-		return newInt
-		
+        else if(num == '-'){
+            var next = tokens.shift();
+            tokens.unshift('(','0','-',next,')');
+            num = read_operand(tokens);
+            return num;
+        }
+        else if(num == "sin("){
+            num = Math.sin(evaluate(tokens));
+            if(tokens.shift() != ')') throw "mismatched";
+            return num;
+        }
+        else if(num == "log("){
+            num = Math.log(evaluate(tokens));
+            if(tokens.shift() != ')') throw "mismatched";
+            return num; 
+        }
+        else if(num == "sqrt("){
+            num = Math.sqrt(evaluate(tokens));
+            if(tokens.shift() != ')') throw "mismatched";
+            return num; 
+        }
+        else{
+            num = parseFloat(num,10);
+            if(isNaN(num)) throw "number expected";
+            return num;
+        }
+    }
+    catch(err){
+        return err;
+    }
 }
 
-//identifies operands and performs operations
-function evaluate(tokens) {
-    try {
-        if(tokens.length<1) throw "Missing operand.";
-    }
-    catch(err) {
-        return "Error: "+err;
-    }
+function evaluate(tokens){
+        if(tokens.length === 0) throw "missing operand";
+        var value = read_term(tokens);
+        var operator = 0;
+        var temp = 0;
+        while(tokens.length > 0){
+            if(tokens[0] == ')') return value;
+            operator = tokens.shift();
+            if(['+','-','×','÷'].indexOf(operator) < 0) throw "unrecognized operator";
+            if(tokens.length === 0) throw "missing operand";
+            temp = read_term(tokens);
+            if(operator === '+'){
+                value = value+temp;
+            } 
+            if(operator === '-'){
+                value = value-temp;
+            }
+        }
+    return value;
+}
+
+function read_term(tokens){
     var value = read_operand(tokens);
-	if(isNaN(value)) return value;
-    while (tokens.length>0) {
-        var operator = tokens.shift();
-        if (operator==")") {
-            return value;
+    var operator = 0;
+    var temp = 0;
+    while(tokens.length >0){
+        if(tokens[0] == ')') return value;
+        operator = tokens.shift();
+        if(['+','-'].indexOf(operator) > -1){
+            tokens.unshift(operator);
+            break;
         }
-        try {
-            if(operator!='+' && operator!='-' && operator!='*' && operator!='/') throw "Unrecognized operator.";
-            if(tokens.length<1) throw "Missing operand.";
+        temp = read_operand(tokens);
+        if(operator === '×'){
+            value = value*temp;
         }
-        catch(err) {
-            return "Error: "+err;
+        if(operator === '÷'){
+            value = value/temp;
         }
-        var temp = read_operand(tokens);
-		try {
-			if(operator=='+') {
-				value = value+temp;
-			} else if(operator=='-') {
-				value = value-temp;
-			} else if(operator=='*') {
-				value = value*temp;
-			} else if(operator=='/') {
-				value = value/temp;
-			}
-			if(isNaN(value)) throw "Number expected";
-		}
-		catch(err) {
-			return "Error: "+err;
-		}
     }
     return value;
 }
+
 $(document).ready(function() {
     $('#tallButton').bind('click', function(){
         var expression = $('#expression');
         var input = expression.text().trim();
-        console.log(expression.text().trim())
-        expression.text(String(calculate(String(input))));
+        expression.text(String(calculate(input)));
+    });
+    
+    $('.button').bind('click',function() {
+        var func = $(this).html();
+        switch (func) {
+            case "MC":
+                console.log("MC");
+                break;
+            case "M+":
+                break;
+            case "M-":
+                break;
+            case "MR":
+                break;
+            case "C":
+                $('#expression').text("");
+                break;
+            case "±":
+                break;
+            case "=":
+                break;
+            default:
+                var expression = $('#expression');
+                var input = expression.text().trim();
+                expression.text(input.concat(func));
+        }
     });
 });
