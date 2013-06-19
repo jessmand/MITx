64559b1403531e0d209d5f9e250d62215a872643
Jessica Andersen <jessmand@mit.edu>
2013-06-19 11:00:20 -0400

    vertical bar chart

--- day8-visual/visual.js
+++ day8-visual/visual.js
@@ -7,23 +7,27 @@ var data = [ 0, 4, 8, 8, 15, 16, 23, 42 ];
 //    chart.append($("<div></div>").css("width", d * 10 + "px").text(d)); 
 //});
 
-var chartHeight = 140;
+var chartHeight = 300;
+var chartWidth = 300;
 
-var xScale = d3.scale.linear().domain([0, d3.max(data)]).range(["0%", "100%"]);
-var yScale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, chartHeight]);
+var yScale = d3.scale.linear().domain([0, d3.max(data)]).range([0, chartHeight]);
+var xScale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, chartWidth]);
 
-var chart = d3.select(".chart-container").append("svg").attr("class", "chart");
+var chart = d3.select(".chart-container").append("svg")
+    .attr("class", "chart")
+    .attr("height", chartHeight)
+    .attr("width", chartWidth);
 
 chart.selectAll("rect").data(data).enter().append("rect")
-    .attr("y", function (d, i) { return yScale(i); })
-    .attr("width", xScale)
-    .attr("height", 20);
+    .attr("x", function (d, i) { return xScale(i); })
+    .attr("y", function (d) { return chartHeight - yScale(d); })
+    .attr("width", xScale.rangeBand())
+    .attr("height", yScale);
 
 chart.selectAll("text").data(data)
     .enter().append("text")
-    .attr("x", xScale)
-    .attr("y", function (d, i) { return yScale(i) + yScale.rangeBand() / 2; })
-    .attr("dx", -3)
-    .attr("dy", "0.35em")
-    .attr("text-anchor", "end")
+    .attr("y", function (d) { return chartHeight - yScale(d) + 3; })
+    .attr("x", function (d, i) { return xScale(i) + xScale.rangeBand() / 2; })
+    .attr("dy", "0.7em")
+    .attr("text-anchor", "middle")
     .text(function (d) { return d; });
