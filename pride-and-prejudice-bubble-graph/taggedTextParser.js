f0cddfd2133184c1144ac24911f39a7231d166bc
Jessica Andersen <jessmand@mit.edu>
2013-06-19 16:41:22 -0400

    no punctuation, proper casing

--- pride-and-prejudice-bubble-graph/taggedTextParser.js
+++ pride-and-prejudice-bubble-graph/taggedTextParser.js
@@ -24,18 +24,30 @@ function checkForWord(tagName,word) {
     return [i];
 }
 
+function punctuation(word) {
+    return (word == "." || word == "," || word == "!" || word == "?" || word == "``" || word == "''" || word == ":" || word == ";" || word == "");
+}
+
 function parseTaggedWord(taggedWord) {
     var index = taggedWord.indexOf("_");
     var word = taggedWord.substring(0,index);
-    var tagName = taggedWord.substring(index);
-    if (!checkForTag(tagName)) {
-        words.children.push({"name":tagName, "children":[{"name":word, "size":1}]});
-    } else {
-        var indices = checkForWord(tagName,word);
-        if (indices.length == 1) {
-            words.children[indices[0]].children.push({"name":word, "size":1});;
+    var tagName = taggedWord.substring(index+1);
+    if (tagName == "nnp") {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+    }
+    if (word == "i") {
+        word = "I";
+    }
+    if (!punctuation(word)) {
+        if (!checkForTag(tagName)) {
+            words.children.push({"name":tagName, "children":[{"name":word, "size":1}]});
         } else {
-            words.children[indices[0]].children[indices[1]].size += 1;
+            var indices = checkForWord(tagName,word);
+            if (indices.length == 1) {
+                words.children[indices[0]].children.push({"name":word, "size":1});;
+            } else {
+                words.children[indices[0]].children[indices[1]].size += 1;
+            }
         }
     }
 }
