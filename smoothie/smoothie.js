15b3a5f4cf4b67276b3e32de91299e80bee36283
Jessica Andersen <jessmand@mit.edu>
2013-06-20 23:05:47 -0400

    basic functionality of smoothie mixing and graphing

--- smoothie/smoothie.js
+++ smoothie/smoothie.js
@@ -8,10 +8,13 @@ var smoothieBlender = (function() {
     //names of fruit
     var fruitArray = {apple:[originalYumminess.apple, 1.5, "#E2342D", "sweet", "chunky", "conventional"], banana:[originalYumminess.banana, .5, "#FFE971", "sweet", "smooth", "tropical"], blueberry:[originalYumminess.blueberry, 2, "#6897FF", "sweet", "chunky", "conventional"], cherry:[originalYumminess.cherry, 3, "#FF3233", "sweet", "chunky", "conventional"], grape:[originalYumminess.grape, 4, "#B171CB", "sweet", "chunky", "conventional"], lemon:[originalYumminess.lemon, .25, "#FFEF1D", "sour", "smooth", "conventional"], lime:[originalYumminess.lime, .5, "#7AC50D", "sour", "smooth", "conventional"], mango:[originalYumminess.mango, 6, "#FFF03F", "sweet", "smooth", "tropical"], orange:[originalYumminess.orange, 1, "#F6A122", "sweet", "smooth", "tropical"], peach:[originalYumminess.peach, 2, "#FFC77B", "sweet", "smooth", "conventional"], pear:[originalYumminess.pear, 1, "#DDE551", "sweet", "chunky", "conventional"], pineapple:[originalYumminess.pineapple, 2, "#FFDA08", "sweet", "chunky", "tropical"], raspberry:[originalYumminess.raspberry, 1.5, "#EC3414", "sour", "chunky", "conventional"], strawberry:[originalYumminess.strawberry, 7, "#EA3927", "sweet", "smooth", "conventional"]};
     
-    
+    var data = [];
+    var fruits = [];
+    var fruitYums = [];
+    var fruitColors = [];
     
     //maximum price set by user
-    var price;
+    var price = 0;
     
     var scenarios = [{text: "red fruits are poisonous", fruit:["apple", "cherry", "raspberry", "strawberry"], effect:"none"}, {text: "you're low on vitamin c! use an orange", fruit:["orange"], effect:"req"}, {text: "limes are extra expensive today", fruit:["lime"], effect:"expensive"}];
     
@@ -34,7 +37,7 @@ var smoothieBlender = (function() {
         
         for (var fruit in fruitArray) {
             
-            fruitList.append($("<li><img src='"+fruit+".png' data-yumminess='"+fruitArray[fruit][0]+"' data-cost='"+fruitArray[fruit][1]+"' data-name='"+fruit+"' class='fruit-tile'><span class='fruit-label'>"+fruit+"<br>"+fruitArray[fruit][0]+" yums, "+monify(fruitArray[fruit][1])+"</span></li>"));
+            fruitList.append($("<li><img src='"+fruit+".png' data-yumminess='"+fruitArray[fruit][0]+"' data-cost='"+fruitArray[fruit][1]+"' data-name='"+fruit+"' data-color='"+fruitArray[fruit][2]+"' class='fruit-tile'><span class='fruit-label'>"+fruit+"<br>"+fruitArray[fruit][0]+" yums, "+monify(fruitArray[fruit][1])+"</span></li>"));
             
         }
         $(".fruitsSpan").empty()
@@ -77,7 +80,7 @@ var smoothieBlender = (function() {
     function addColor(div) {
         
         div.append($("<div class='row-fluid'>favorite color</div>"));
-        div.append($("<div class='row-fluid'><div class='span2 redSlider'></div><div class='span2 greenSlider'></div><div class='span2 blueSlider'></div><div class='span6 swatch'></div></div>"))
+        div.append($("<div class='row-fluid'><div class='span2 redSlider'></div><div class='span2 greenSlider'></div><div class='span2 blueSlider'></div><div class='span6'><div class='swatch'></div></div></div>"))
         
     }
     
@@ -151,6 +154,8 @@ var smoothieBlender = (function() {
         var favoriteColor = stringToRgb($(".swatch").css("background-color"));
         
         
+        $('.maxCost').text(monify(price));
+        
         for (var fruit in fruitArray) {
             
             var fruitColor = hexToRgb(fruitArray[fruit][2]);
@@ -212,27 +217,109 @@ var smoothieBlender = (function() {
         
         addFruitList();
         addSmoothieList();
-        $( "#fruits, #smoothie" ).sortable({
-          connectWith: ".connectedSortable", handle: "img", update: updateStatus
+        $( "#fruits" ).sortable({
+          connectWith: ".connectedSortable", handle: "img", update: updateStatus, revert: 250
         }).disableSelection();
+        $( "#smoothie" ).sortable({
+          connectWith: ".connectedSortable", handle: "img", update: updateStatus, receive: checkReceive, revert: 250
+        }).disableSelection();
+    }
+    
+    function checkReceive() {
+        var smoothieFruits = $(".smoothieSpan").find(".fruit-tile");
+        var totalCost = 0;
+        smoothieFruits.each(function() {
+            totalCost += parseFloat($(this).attr("data-cost"));
+        });
+        if (totalCost>price) {
+            newAlert("don't go over your max cost!");
+            $('.connectedSortable').sortable("cancel");
+        }
     }
     
     function updateStatus() {
         var smoothieFruits = $(".smoothieSpan").find(".fruit-tile");
         var totalCost = 0;
         var totalYums = 0;
+        
+        data = [];
+        fruits = [];
+        fruitYums = [];
+        fruitColors = [];
+        
         smoothieFruits.each(function() {
             totalCost += parseFloat($(this).attr("data-cost"));
             totalYums += parseInt($(this).attr("data-yumminess"));
+            data.push([{y:parseFloat($(this).attr("data-cost"))}]);
+            fruits.push($(this).attr("data-name"));
+            fruitYums.push($(this).attr("data-yumminess"));
+            fruitColors.push($(this).attr("data-color"));
         });
+        
+        graph();
+        
         $(".totalCost").text(monify(totalCost));
         $(".totalYums").text(totalYums);
         $("#smoothie").css("height", $("#fruits").css("height"));
+        
     }
+    
+      function graph(){
+        $(".chart-container").empty();
+        var maxPrice = $("#hungerSlider").slider("value");
+        var outer_width = parseInt($(".statusSpan").css("width"))-38;
+        var outer_height = 300;
+        
+        var margin = { top: outer_width/8, right: outer_width/8, bottom: outer_width/8, left: outer_width/8 }
+        var chart_width = outer_width - margin.left - margin.right;
+        var chart_height = outer_height -margin.top - margin.bottom;
+                
+        var y_scale = d3.scale.linear().domain([0,maxPrice]).range([chart_height,0]);
+        
+        var chart = d3.select(".chart-container").append("svg").attr("class","chart").attr("height", outer_height).attr("width",outer_width).append("g").attr("transform","translate(" + (margin.left+margin.right) + "," + (margin.top + margin.bottom -5)+ ")");
+            
+        chart.selectAll(".y-scale-label").data(y_scale.ticks(4)).enter().append("text").attr("class", "y-scale-label").attr("x",0).attr('y',y_scale).attr("text-anchor","end").attr("dy","0.3em").attr("dx",-margin.left/2).text(function(d){return d/maxPrice*100 + "%"});
+        
+        if(data.length>0){
+            var stack = d3.layout.stack();
+            var stacked_data = stack(data);
+
+            var color_scale = function(index){
+                return fruitColors[index];
+            }
+            
+            var layer_groups = chart.selectAll(".layer").data(stacked_data).enter().append("g").attr("class", "layer");
+            
+            var rects = layer_groups.selectAll('rect').data(function(d){return d}).enter().append('rect').attr("x",0).attr("y", function(d){return y_scale(d.y0+d.y)}).attr("width", chart_width).attr("height", function(d){ return y_scale(d.y0) - y_scale(d.y0+d.y); }).style("fill", function(d, i, j) { return color_scale(j);});
+            
+            var label_groups = chart.selectAll(".label").data(stacked_data).enter().append("g").attr("class", "label");
+            
+            label_groups.selectAll(".bar-label").data(function(d){return d;}).enter().append("text").attr("class", "bar-label").attr("x",chart_width/2).attr("y", function(d){return y_scale(d.y0+d.y/2);}).attr("text-anchor", "middle").text(function(d,i,j){return fruits[j] + ": $"+d.y+", "+fruitYums[j]+" yums";});
+            
+        }
+        
+      }
     
     $(window).resize(function() {
         $("#smoothie").css("height", $("#fruits").css("height"));
+        graph();
     });
+    
+    function sameSizeUl() {
+    
+        var smoothieTiles = $("#smoothie").find("li").length;;
+        var fruitsTiles = $("#fruits").find("li").length;
+        
+        if (smoothieTiles<fruitsTiles) {
+            $("#fruits").css("height", "");
+            $("#smoothie").css("height", $("#fruits").css("height"));
+        } else if (smoothieTiles>fruitsTiles) {
+            $("#smoothie").css("height", "");
+            $("#fruits").css("height", $("#smoothie").css("height"));
+            
+        }
+    
+    }
     
     function newAlert(warningText) {
         $(".alertRow").append($('<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">&times;</button>'+warningText+'</div>'))
@@ -240,7 +327,7 @@ var smoothieBlender = (function() {
     }
     
     function addStatus(div) {
-        div.append($("<div>total yumminess: <span class='totalYums'>0</span></div><div>total cost: <span class='totalCost'>$0</span></div><button class='btn blendSmoothieButton'>blend smoothie</div>"));
+        div.append($("<div>total yumminess: <span class='totalYums'>0</span></div><div>total cost: <span class='totalCost'>$0</span></div><button class='btn blendSmoothieButton'>blend smoothie</button><div>max cost: <span class='maxCost'>$0</span></div><div class='chart-container'></div></div>"));
     }
     
     function blendSmoothie() {
@@ -259,7 +346,7 @@ var smoothieBlender = (function() {
         var applySpan = $("<div class='span2 applySpan well'></div>");
         var fruitsSpan = $("<div class='span4 fruitsSpan well'></div>");
         var smoothieSpan = $("<div class='span4 smoothieSpan well'></div>");
-        var statusSpan = $("<div class='span2 well'></div>");
+        var statusSpan = $("<div class='span2 well statusSpan'></div>");
         addApply(applySpan);
         addStatus(statusSpan);
         dragAndDropRow.append(applySpan, fruitsSpan, smoothieSpan, statusSpan);
@@ -282,7 +369,8 @@ var smoothieBlender = (function() {
         
         
         
-        $("#sweetnessSlider, #smoothnessSlider, #tropicalitySlider, #hungerSlider").slider({min: 1, max: 10, value: 5});
+        $("#sweetnessSlider, #smoothnessSlider, #tropicalitySlider").slider({min: 1, max: 10, value: 5});
+        $("#hungerSlider").slider({min: 2, max: 20, value: 10});
         
         $( ".redSlider, .greenSlider, .blueSlider" ).slider({
           orientation: "vertical",
@@ -300,8 +388,7 @@ var smoothieBlender = (function() {
         $(".redSlider").find(".ui-slider-handle").css("border", "1px solid red");
         $(".greenSlider").find(".ui-slider-handle").css("border", "1px solid green");
         $(".blueSlider").find(".ui-slider-handle").css("border", "1px solid blue");
-        
-        
+        $(".swatch").css("height", (parseInt($(".redSlider").css("height").substring(0, $(".redSlider").css("height").indexOf("p")))-10)+"px");
         $(".blendSmoothieButton").on("click",blendSmoothie);
     };
     
